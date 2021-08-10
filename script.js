d4ec0b1c20d9b0c5ed279c337a632619ff803e80
Patrick Brown <breepatbree@gmail.com>
2021-08-09 23:08:45 -0600

    Found json object paths

--- script.js
+++ script.js
@@ -15,7 +15,59 @@
 // WHEN I click on a city in the search history
 // THEN I am again presented with current and future conditions for that city
 
+
+//API KEY
 const APIKey = "54123fbf0da279d26be1750e4bf7181c";
+//Search bar
+const searchEl = document.getElementById("searchInput").val;
 
-const searchEl = document.getElementById("searchInput");
+// https://api.openweathermap.org/data/2.5/onecall?lat={lat}&lon={lon}&exclude={part}&appid={API key}
 
+
+///CITY NAME
+// https://api.openweathermap.org/data/2.5/forecast?q=denver&appid=54123fbf0da279d26be1750e4bf7181c
+
+$("#citySearchBtn").on('click', function(){
+    event.preventDefault();
+            ///Change DENVER to citySearch value and concat
+    fetch('https://api.openweathermap.org/data/2.5/forecast?q=denver&units=imperial&cnt=40&appid=54123fbf0da279d26be1750e4bf7181c')
+    // fetch('https://api.openweathermap.org/data/2.5/forecast/daily?q=denver&cnt=5&appid=d5da65a1b7938f90d55e1d6961ade256')
+
+    .then(function (response){
+        return response.json();
+    })
+
+    .then(function (data){
+        console.log(data)
+
+        ///if statement for i down there
+        //////////DAY 0
+        console.log("Day 0----------------------------")
+        //city
+        console.log(data.city.name);
+
+        //icon, ICON URL openweathermap.org/img/wn/{THISDATAHERE}@2x.png
+        console.log(data.list[0].weather[0].icon);
+
+        //temp
+        console.log(data.list[0].main.temp)
+
+        //humidity
+        console.log(data.list[0].main.humidity);
+
+        //wind
+        console.log(data.list[0].wind.speed);
+
+    })
+
+});
+
+
+
+
+
+
+    // $.ajax({url:'api.openweathermap.org/data/2.5/forecast?q=denver&appid=54123fbf0da279d26be1750e4bf7181c',
+    //         method: "GET",})
+
+    // )});
