00c0a7648c5e1de23686f8e2c406111ec5f9145a
Patrick Brown <breepatbree@gmail.com>
2021-08-10 09:34:59 -0600

    Cards populate with weather data

--- script.js
+++ script.js
@@ -15,59 +15,74 @@
 // WHEN I click on a city in the search history
 // THEN I am again presented with current and future conditions for that city
 
-
-//API KEY
-const APIKey = "54123fbf0da279d26be1750e4bf7181c";
 //Search bar
-const searchEl = document.getElementById("searchInput").val;
-
-// https://api.openweathermap.org/data/2.5/onecall?lat={lat}&lon={lon}&exclude={part}&appid={API key}
-
-
-///CITY NAME
-// https://api.openweathermap.org/data/2.5/forecast?q=denver&appid=54123fbf0da279d26be1750e4bf7181c
+let searchEl 
 
 $("#citySearchBtn").on('click', function(){
     event.preventDefault();
-            ///Change DENVER to citySearch value and concat
-    fetch('https://api.openweathermap.org/data/2.5/forecast?q=denver&units=imperial&cnt=40&appid=54123fbf0da279d26be1750e4bf7181c')
-    // fetch('https://api.openweathermap.org/data/2.5/forecast/daily?q=denver&cnt=5&appid=d5da65a1b7938f90d55e1d6961ade256')
-
+    var searchEl = $("#searchInput").val()
+fetch('https://api.openweathermap.org/data/2.5/forecast?q='+searchEl+'&units=imperial&cnt=40&appid=eabfc1685d3a5a1ea93166e8b31eb771')
     .then(function (response){
         return response.json();
     })
 
     .then(function (data){
-        console.log(data)
+        // console.log(data);
+        // ///if statement for i down there
+        // //////////DAY 0
+        // console.log("Day 0----------------------------")
 
-        ///if statement for i down there
-        //////////DAY 0
-        console.log("Day 0----------------------------")
-        //city
-        console.log(data.city.name);
+        // console.log(data.list[0].dt_txt)
+        // //city
+        // console.log(data.city.name);
 
-        //icon, ICON URL openweathermap.org/img/wn/{THISDATAHERE}@2x.png
-        console.log(data.list[0].weather[0].icon);
+        // //icon, ICON URL openweathermap.org/img/wn/{THISDATAHERE}@2x.png
+        // console.log(data.list[0].weather[0].icon);
 
-        //temp
-        console.log(data.list[0].main.temp)
+        // //temp
+        // console.log(data.list[0].main.temp)
 
-        //humidity
-        console.log(data.list[0].main.humidity);
+        // //humidity
+        // console.log(data.list[0].main.humidity);
 
-        //wind
-        console.log(data.list[0].wind.speed);
+        // //wind
+        // console.log(data.list[0].wind.speed);
+        // // var Icon = data.list[0].weather[0].icon;
+
+        $(".card").removeClass('hide')
+///////////////CREATES WEATHER CARDS
+        ///DAY 0
+        $("#day0Img").attr("src","http://openweathermap.org/img/wn/" + data.list[0].weather[0].icon +".png")
+        $("#day0Card h5").text(data.list[0].dt_txt)
+        $("#cityName").text(data.city.name)
+        $("#temperature0").text("Temperature: "+ data.list[0].main.temp+"F")
+        $("#humidity0").text("Humidity: "+data.list[0].main.humidity)
+        $("#wind0").text("Wind Speed: "+ data.list[0].main.humidity+" mpg")
+        /////DAY 1
+        $("#day1Img").attr("src","http://openweathermap.org/img/wn/"+data.list[8].weather[0].icon+".png")
+        $("#card-title-1").text(data.list[8].dt_txt)
+        $("#temperature1").text("Temperature: "+ data.list[8].main.temp+"F")
+        $("#humidity1").text("Humidity: "+data.list[8].main.humidity)
+        $("#wind1").text("Wind Speed: "+ data.list[8].main.humidity+" mpg")
+        ///Day 2
+        $("#day2Img").attr("src","http://openweathermap.org/img/wn/"+data.list[16].weather[0].icon+".png")
+        $("#card-title-2").text(data.list[16].dt_txt)
+        $("#temperature2").text("Temperature: "+ data.list[16].main.temp+"F")
+        $("#humidity2").text("Humidity: "+data.list[16].main.humidity)
+        $("#wind2").text("Wind Speed: "+ data.list[16].main.humidity+" mpg")
+        ///Day 3
+        $("#day3Img").attr("src","http://openweathermap.org/img/wn/"+data.list[24].weather[0].icon+".png")
+        $("#card-title-3").text(data.list[24].dt_txt)
+        $("#temperature3").text("Temperature: "+ data.list[24].main.temp+"F")
+        $("#humidity3").text("Humidity: "+data.list[24].main.humidity)
+        $("#wind3").text("Wind Speed: "+ data.list[24].main.humidity+" mpg")
+        //Day 4
+        $("#day4Img").attr("src","http://openweathermap.org/img/wn/"+data.list[32].weather[0].icon+".png")
+        $("#card-title-4").text(data.list[32].dt_txt)
+        $("#temperature4").text("Temperature: "+ data.list[32].main.temp+"F")
+        $("#humidity4").text("Humidity: "+data.list[32].main.humidity)
+        $("#wind4").text("Wind Speed: "+ data.list[32].main.humidity+" mpg")        
 
     })
 
 });
-
-
-
-
-
-
-    // $.ajax({url:'api.openweathermap.org/data/2.5/forecast?q=denver&appid=54123fbf0da279d26be1750e4bf7181c',
-    //         method: "GET",})
-
-    // )});
